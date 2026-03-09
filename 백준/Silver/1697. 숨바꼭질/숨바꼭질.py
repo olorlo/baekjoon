e433aa6b8a1f3abb6901b556555bb642058b39a8
이영은 <yongeuni22@gmail.com>
2026-03-09 09:17:55 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 37836 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,31 @@
+import sys
+# sys.stdin = open("input.txt", "r")
+
+from collections import deque
+
+# 1697번 숨바꼭질
+def bfs(n,k):
+    dq = deque([n])
+    visited[n] = 0
+
+    while dq:
+        subin = dq.popleft()
+
+        if subin == k:
+            return visited[subin]
+    
+        if 0 <= subin*2<=100000 and visited[subin*2] == -1:
+            visited[subin*2] = visited[subin] + 1
+            dq.append(subin*2)
+
+        if 0 <= subin-1<=100000 and visited[subin-1] == -1:
+            visited[subin -1] = visited[subin] + 1
+            dq.append(subin-1)
+
+        if 0 <= subin+1 <=100000 and visited[subin+1] == -1:
+            visited[subin +1] = visited[subin] + 1
+            dq.append(subin+1)
+ 
+N, K = map(int,input().split())
+visited = [-1] * 100001
+print(bfs(N, K))
